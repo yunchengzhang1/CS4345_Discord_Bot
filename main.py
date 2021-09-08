a85c481cb5ab08fb3af720fefa5c9fdf18852358
Austin Liu <37671748+ostenloo@users.noreply.github.com>
2021-09-07 21:00:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import os
+import discord
+from dotenv import load_dotenv
+from discord.ext import commands
 
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+bot = discord.Client()
+
+bot = commands.Bot(command_prefix='!', help_command=None)
+@bot.command(name="test")
+async def test(ctx):
+    await ctx.send("hello world")
+
+bot.run(TOKEN)
