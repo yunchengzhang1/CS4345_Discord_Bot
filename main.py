1e5c25001a3cdfe1ef8aa8455eb7714e385d976e
Edward-jiang495 <ejiang@smu.edu>
2021-10-06 11:26:18 -0500

    database methods added

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
+import mysql.connector
+from mysql.connector import Error
 import os
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
+from database_func import database_func
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-bot = discord.Client()
 
-bot = commands.Bot(command_prefix='!', help_command=None)
-@bot.command(name="test")
-async def test(ctx):
-    await ctx.send("hello world")
+test = database_func()
+test.users_in_class(3)
+# connect to database
 
-bot.run(TOKEN)
+# load_dotenv()
+# TOKEN = os.getenv('DISCORD_TOKEN')
+#
+#
+# bot = commands.Bot(command_prefix='!', help_command=None)
+# # @bot.command(name="test")
+# # async def test(ctx):
+# #     await ctx.send("Edward Testing")
+# #
+# @bot.command(name="ping")
+# async def ping(ctx):
+#     await ctx.send("Latency: " +str(round(bot.latency*1000)) +" ms")
+#
+# bot.run(TOKEN)
+
