12296e460de9e173d6d65e75eff38af730853275
Edward-jiang495 <ejiang@smu.edu>
2021-10-17 23:20:26 -0500

    poll added

--- main.py
+++ main.py
@@ -16,33 +16,5 @@ for file in os.listdir("./cogs"):
 bot.run(TOKEN)
 
 
-# a polling feature that Edward is working on
-# @bot.command(name="createpoll", aliases=["mkpoll"])
-# async def create_poll(ctx, hours: int, question: str, *options):
-#     polls = []
-#     if len(options) > 10:
-#         await ctx.send("You can only supply a maximum of 10 options.")
-#
-#     else:
-#         embed = Embed(title="Poll",
-#                       description=question,
-#                       colour=ctx.author.colour,
-#                       timestamp=datetime.utcnow())
-#
-#         fields = [("Options", "\n".join([f"{numbers[idx]} {option}" for idx, option in enumerate(options)]), False),
-#                   ("Instructions", "React to cast a vote!", False)]
-#
-#         for name, value, inline in fields:
-#             embed.add_field(name=name, value=value, inline=inline)
-#
-#         message = await ctx.send(embed=embed)
-#
-#         for emoji in numbers[:len(options)]:
-#             await message.add_reaction(emoji)
-#
-#         polls.append((message.channel.id, message.id))
-#
-#         bot.scheduler.add_job(self.complete_poll, "date", run_date=datetime.now() + timedelta(seconds=hours),
-#                                    args=[message.channel.id, message.id])
-#
+
 # bot.run(TOKEN)
